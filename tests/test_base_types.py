c9ce5f46ac72f6a83805a14624385ad5946633df
Jake Barry <jake.barry@peopledatalabs.com>
2023-08-11 09:54:52 -0700

    Merging changes from main

--- tests/test_base_types.py
+++ tests/test_base_types.py
@@ -1,8 +1,6 @@
 from datetime import date, datetime, timedelta
 from decimal import Decimal
 
-from hypothesis import given
-from hypothesis import strategies as st
 from pyspark.sql.types import (
     BinaryType,
     BooleanType,
@@ -33,24 +31,7 @@ class RawValuesModel(SparkModel):
     hh: DoubleType
 
 
-raw_values_strategy = st.fixed_dictionaries(
-    {
-        'a': st.integers(min_value=1, max_value=1000),
-        'b': st.floats(min_value=0, max_value=5),
-        'c': st.text(min_size=1, max_size=20),
-        'd': st.booleans(),
-        'e': st.binary(max_size=100),
-        'f': st.decimals(min_value=0, max_value=1000, allow_nan=False, allow_infinity=False),
-        'y': st.dates(),
-        'cc': st.datetimes(),
-        'gg': st.timedeltas(),
-        'hh': st.builds(DoubleType)
-    }
-)
-
-
-@given(raw_values_strategy)
-def test_raw_values(data):
+def test_raw_values():
     expected_schema = StructType(
         [
             StructField('a', IntegerType(), False),
@@ -62,9 +43,8 @@ def test_raw_values(data):
             StructField('y', DateType(), False),
             StructField('cc', TimestampType(), False),
             StructField('gg', DayTimeIntervalType(0, 3), False),
-            StructField('hh', DoubleType(),  False)
+            StructField('hh', DoubleType(), False),
         ]
     )
-    user = RawValuesModel(**data)
-    generated_schema = user.model_spark_schema()
+    generated_schema = RawValuesModel.model_spark_schema()
     assert generated_schema == expected_schema
--- tests/test_list_types.py
+++ tests/test_list_types.py
@@ -2,8 +2,6 @@ from datetime import date, datetime, timedelta
 from decimal import Decimal
 from typing import List, Optional
 
-from hypothesis import given
-from hypothesis import strategies as st
 from pyspark.sql.types import (
     ArrayType,
     BinaryType,
@@ -40,27 +38,7 @@ class ListValuesModel(SparkModel):
     o1: Optional[List[MyModel]]
 
 
-list_values_strategy = st.fixed_dictionaries(
-    {
-        'm': st.lists(st.integers(min_value=1, max_value=1000)),
-        'n': st.lists(st.floats(min_value=0, max_value=5)),
-        'o': st.lists(st.text(min_size=1, max_size=20)),
-        'p': st.lists(st.booleans()),
-        'q': st.lists(st.binary(max_size=100)),
-        'r': st.lists(
-            st.decimals(min_value=0, max_value=1000, allow_nan=False, allow_infinity=False)
-        ),
-        'aa': st.lists(st.dates()),
-        'ee': st.lists(st.datetimes()),
-        'ii': st.lists(st.timedeltas()),
-        'oo': st.lists(st.builds(MyModel, k=st.text(min_size=1, max_size=20))),
-        'o1': st.lists(st.builds(MyModel, k=st.text(min_size=1, max_size=20)))
-    }
-)
-
-
-@given(list_values_strategy)
-def test_list_values(data):
+def test_list_values():
     expected_schema = StructType(
         [
             StructField('m', ArrayType(IntegerType(), False), False),
@@ -76,6 +54,5 @@ def test_list_values(data):
             StructField('o1', ArrayType(StructType([StructField("k", StringType(), False)]), True), True),
         ]
     )
-    user = ListValuesModel(**data)
-    generated_schema = user.model_spark_schema()
+    generated_schema = ListValuesModel.model_spark_schema()
     assert generated_schema == expected_schema
