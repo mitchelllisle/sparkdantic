a35660796393bf51ce2f66d17649833c788fb9d9
Mitchell Lisle <mitchelllisle@mailbox.org>
2024-07-20 20:38:41 +1000

    add test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from faker import Faker
 from pyspark.sql import SparkSession
 
 
@@ -8,8 +7,3 @@ def spark():
     spark_session = SparkSession.builder.appName('unit tests').getOrCreate()
     yield spark_session
     spark_session.stop()
-
-
-@pytest.fixture(scope='session')
-def faker() -> Faker:
-    return Faker()
