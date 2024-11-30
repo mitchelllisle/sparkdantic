44e2e884cf566fc4d4582e5b2e57adcf4343f5e0
Mitchell Lisle <mitchelllisle@mailbox.org>
2024-11-30 15:27:18 +1100

    change test to validate spark type force

--- tests/test_spark_field.py
+++ tests/test_spark_field.py
@@ -4,13 +4,13 @@ from sparkdantic import SparkField, SparkModel
 
 
 class Model(SparkModel):
-    z: int = SparkField(spark_type=T.IntegerType, alias='_z')
+    z: int = SparkField(spark_type=T.StringType, alias='_z')
 
 
 def test_spark_field():
     schema = Model.model_spark_schema()
     assert schema == T.StructType(
         [
-            T.StructField('_z', T.IntegerType(), False),
+            T.StructField('_z', T.StringType(), False),
         ]
     )
