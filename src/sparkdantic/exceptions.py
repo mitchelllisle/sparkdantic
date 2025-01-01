77185acf058ffcd7970cdb24d655460d4bb91efd
mitchstockdale <mitchellastockdale@gmail.com>
2025-01-02 08:13:48 +1100

    Require pyspark version in range
    Use packaging library for version check as
    disutils is removed in python 3.12.

--- src/sparkdantic/exceptions.py
+++ src/sparkdantic/exceptions.py
@@ -1,2 +1,8 @@
 class TypeConversionError(Exception):
     """Error converting a model field type to a PySpark type"""
+
+
+class SparkdanticImportError(ImportError):
+    """
+    Wrapper class for ImportError to support error classes.
+    """
